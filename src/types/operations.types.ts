dc53bc2351c04a247ca593b1f2ddc64af2083fea
yakovenkomax <yakovenkomax@outlook.com>
2020-07-27 22:43:24 +0300

    Add pinned items loading

--- src/types/operations.types.ts
+++ src/types/operations.types.ts
@@ -1,4 +1,13 @@
-import { Exact, Maybe, Organization, PageInfo, Repository, Scalars } from 'types/schema.types';
+import {
+  Exact, Gist,
+  Language, License,
+  Maybe,
+  Organization,
+  PageInfo,
+  Repository, RepositoryConnection,
+  Scalars,
+  StargazerConnection,
+} from 'types/schema.types';
 
 export type GetOrganizationQueryVariables = Exact<{
   login: Scalars['String'];
@@ -9,14 +18,31 @@ export type GetOrganizationQuery = (
   { __typename?: 'Query' }
   & { organization?: Maybe<(
     { __typename?: 'Organization' }
-    & Pick<Organization, 'name' | 'location' | 'websiteUrl'>
-    & { repositories: (
+    & Pick<Organization, 'name' | 'location' | 'avatarUrl' | 'websiteUrl'>
+    & { pinnedItems: (
+      { __typename?: 'PinnableItemConnection' }
+      & { edges?: Maybe<Array<Maybe<(
+        { __typename?: 'PinnableItemEdge' }
+        & { node?: Maybe<{ __typename?: 'Gist' } | (
+          { __typename?: 'Repository' }
+          & RepositoryFieldsFragment
+          )> }
+        )>>> }
+      ), repositories: (
       { __typename?: 'RepositoryConnection' }
       & { edges?: Maybe<Array<Maybe<(
         { __typename?: 'RepositoryEdge' }
         & { node?: Maybe<(
           { __typename?: 'Repository' }
-          & Pick<Repository, 'id' | 'name'>
+          & Pick<Repository, 'updatedAt'>
+          & { parent?: Maybe<(
+            { __typename?: 'Repository' }
+            & Pick<Repository, 'nameWithOwner'>
+            )>, licenseInfo?: Maybe<(
+            { __typename?: 'License' }
+            & Pick<License, 'spdxId'>
+            )> }
+          & RepositoryFieldsFragment
           )> }
         )>>>, pageInfo: (
         { __typename?: 'PageInfo' }
@@ -24,4 +50,19 @@ export type GetOrganizationQuery = (
         ) }
       ) }
     )> }
+  );
+
+export type RepositoryFieldsFragment = (
+  { __typename?: 'Repository' }
+  & Pick<Repository, 'id' | 'name' | 'description'>
+  & { primaryLanguage?: Maybe<(
+    { __typename?: 'Language' }
+    & Pick<Language, 'color' | 'name'>
+    )>, stargazers: (
+    { __typename?: 'StargazerConnection' }
+    & Pick<StargazerConnection, 'totalCount'>
+    ), forks: (
+    { __typename?: 'RepositoryConnection' }
+    & Pick<RepositoryConnection, 'totalCount'>
+    ) }
   );
