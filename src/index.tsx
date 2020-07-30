d502a858e90a6f252452698442e0c34686e87cbe
yakovenkomax <yakovenkomax@outlook.com>
2020-07-30 10:47:27 +0300

    Move apollo client back to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloLink } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 import App from 'components/App/App';
-import { apolloClient } from 'apolloClient';
-import { ApolloProvider } from '@apollo/client';
 
-import 'global.css';
+import './global.css';
 
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`,
+  },
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+// FIXME: https://github.com/apollographql/apollo-client/issues/6011
+const link: any = ApolloLink.from([errorLink, httpLink]);
+const cache = new InMemoryCache();
+const client = new ApolloClient({ link, cache });
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={apolloClient}>
+    <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
   </React.StrictMode>,
